c947b46f678ec95b410774348d09daf6635dfdb1
D4rksoon <Asdreem2539@gmail.com>
2024-02-29 02:17:17 +0800

    Описание класса
    Подключены основные библиотеки
    Создан класс Counter

--- Example28/widget.h
+++ Example28/widget.h
@@ -2,6 +2,11 @@
 #define WIDGET_H
 
 #include <QWidget>
+#include <QPushButton>
+#include <QLabel>
+#include <QLineEdit>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
 
 class Widget : public QWidget
 {
@@ -10,5 +15,34 @@ class Widget : public QWidget
 public:
     Widget(QWidget *parent = nullptr);
     ~Widget();
+
+protected:
+    QLabel *label1;
+    QLabel *label2;
+    QPushButton *calcButton;
+    QPushButton *exitButton;
+    Counter *edit1;
+    Counter *edit2;
 };
+
+class Counter : public QLineEdit
+{
+public:
+    Counter(const QString & contents, QWidget * parent = nullptr)
+    : QLineEdit(content, parent){}
+signals:
+    void tick_signal();
+
+public slots:
+    void add_one()
+    {
+        QString str = text();
+        int intValue = str.toInt();
+        if(intValue != 0 and intValue % 5 == 0) emit tick_signal();
+        intValue++;
+        str.setNum(intValue);
+        setText(str);
+    }
+};
+
 #endif // WIDGET_H
