dccc042ab8fd3a6f8f81a1fa8af0c985d5cdc6ee
D4rksoon <Asdreem2539@gmail.com>
2024-03-02 02:45:56 +0800

    Описание Widget.cpp
    Создание основного окна приложения

--- Example28/widget.cpp
+++ Example28/widget.cpp
@@ -3,6 +3,36 @@
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
 {
+    setWindowTitle("Счетчик");
+    label1 = new QLabel("Cчет по 1");
+    label2 = new QLabel("Cчет по 5");
+    edit1 = new Counter("0", this);
+    edit2 = new Counter("0", this);
+    calcButton=new QPushButton("+1", this);
+    exitButton=new QPushButton("Выход", this);
+
+    layout1 = new QHBoxLayout();
+    layout1->addWidget(label1);
+    layout1->addWidget(label2);
+
+    layout2 = new QHBoxLayout();
+    layout2->addWidget(edit1);
+    layout2->addWidget(edit2);
+
+    layout3 = new QHBoxLayout();
+    layout3->addWidget(calcButton);
+    layout3->addWidget(exitButton);
+
+    layout4 = new QVBoxLayout(this);
+    layout4->addLayout(layout1);
+    layout4->addLayout(layout2);
+    layout4->addLayout(layout3);
+
+    connect(calcButton, SIGNAL(clicked(bool)), edit1, SLOT(add_one()));
+
+    connect(edit1, SIGNAL(tick_signal()), edit2, SLOT(add_one()));
+
+    connect(exitButton, SIGNAL(clicked(bool)), this,  SLOT(close()));
 }
 
 Widget::~Widget()
--- Example28/widget.h
+++ Example28/widget.h
@@ -8,6 +8,28 @@
 #include <QVBoxLayout>
 #include <QHBoxLayout>
 
+class Counter : public QLineEdit
+{
+    Q_OBJECT
+public:
+    Counter(const QString & contents, QWidget * parent = nullptr)
+    : QLineEdit(contents, parent){}
+
+signals:
+    void tick_signal();
+
+public slots:
+    void add_one()
+    {
+        QString str = text();
+        int intValue = str.toInt();
+        intValue++;
+        str.setNum(intValue);
+        setText(str);
+        if(intValue != 0 and intValue % 5 == 0) emit tick_signal();
+    }
+};
+
 class Widget : public QWidget
 {
     Q_OBJECT
@@ -23,26 +45,12 @@ protected:
     QPushButton *exitButton;
     Counter *edit1;
     Counter *edit2;
+    QHBoxLayout *layout1;
+    QHBoxLayout *layout2;
+    QHBoxLayout *layout3;
+    QVBoxLayout *layout4;
 };
 
-class Counter : public QLineEdit
-{
-public:
-    Counter(const QString & contents, QWidget * parent = nullptr)
-    : QLineEdit(content, parent){}
-signals:
-    void tick_signal();
 
-public slots:
-    void add_one()
-    {
-        QString str = text();
-        int intValue = str.toInt();
-        if(intValue != 0 and intValue % 5 == 0) emit tick_signal();
-        intValue++;
-        str.setNum(intValue);
-        setText(str);
-    }
-};
 
 #endif // WIDGET_H
